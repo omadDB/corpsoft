6ceb451453a956b6b9d97c5044bd4210c309e3ff
omad_d <odilshodvekov13@gmail.com>
2024-08-13 14:45:31 +0500

    Project deployed

---
+++ js/script.js
@@ -0,0 +1,177 @@
+///////////////////////////////////////////////////////////
+// Set current year
+const yearEl = document.querySelector(".year")
+const currentYear = new Date().getFullYear()
+yearEl.textContent = currentYear
+
+//////////////////////////////////////////////////////////
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav")
+const headerEl = document.querySelector(".header")
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open")
+})
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link")
+
+// allLinks.forEach(function (link) {
+//   link.addEventListener("click", function (e) {
+//     e.preventDefault()
+//     const href = link.getAttribute("href")
+
+//     // Scroll back to top
+//     if (href === "#")
+//       window.scrollTo({
+//         top: 0,
+//         behavior: "smooth",
+//       })
+
+//     // Scroll to other links
+//     if (href !== "#" && href.startsWith("#")) {
+//       const sectionEl = document.querySelector(href)
+//       sectionEl.scrollIntoView({ behavior: "smooth" })
+//     }
+
+//     // Close mobile naviagtion
+//     if (link.classList.contains("main-nav-link"))
+//       headerEl.classList.toggle("nav-open")
+//   })
+// })
+
+///////////////////////////////////////////////////////////
+// Sticky navigation
+
+// const sectionHeroEl = document.querySelector(".section-hero")
+
+// const obs = new IntersectionObserver(
+//   function (entries) {
+//     const ent = entries[0]
+//     console.log(ent)
+
+//     if (ent.isIntersecting === false) {
+//       document.body.classList.add("sticky")
+//     }
+
+//     if (ent.isIntersecting === true) {
+//       document.body.classList.remove("sticky")
+//     }
+//   },
+//   {
+//     // In the viewport
+//     root: null,
+//     threshold: 0,
+//     rootMargin: "-80px",
+//   }
+// )
+// obs.observe(sectionHeroEl)
+
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div")
+  flex.style.display = "flex"
+  flex.style.flexDirection = "column"
+  flex.style.rowGap = "1px"
+
+  flex.appendChild(document.createElement("div"))
+  flex.appendChild(document.createElement("div"))
+
+  document.body.appendChild(flex)
+  var isSupported = flex.scrollHeight === 1
+  flex.parentNode.removeChild(flex)
+  console.log(isSupported)
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap")
+}
+checkFlexGap()
+
+///////////////////////////////////////
+// Slider
+const slider = function () {
+  const slides = document.querySelectorAll(".slide")
+  const btnLeft = document.querySelector(".slider__btn--left")
+  const btnRight = document.querySelector(".slider__btn--right")
+  const dotContainer = document.querySelector(".dots")
+
+  let curSlide = 0
+  const maxSlide = slides.length
+
+  // Functions
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotContainer.insertAdjacentHTML(
+        "beforeend",
+        `<button class="dots__dot" data-slide="${i}"></button>`
+      )
+    })
+  }
+
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll(".dots__dot")
+      .forEach((dot) => dot.classList.remove("dots__dot--active"))
+
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add("dots__dot--active")
+  }
+
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${150 * (i - slide)}%)`)
+    )
+  }
+
+  // Next slide
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0
+    } else {
+      curSlide++
+    }
+
+    goToSlide(curSlide)
+    activateDot(curSlide)
+  }
+
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1
+    } else {
+      curSlide--
+    }
+    goToSlide(curSlide)
+    activateDot(curSlide)
+  }
+
+  const init = function () {
+    goToSlide(0)
+    createDots()
+
+    activateDot(0)
+  }
+  init()
+
+  // Event handlers
+  btnRight.addEventListener("click", nextSlide)
+  btnLeft.addEventListener("click", prevSlide)
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") prevSlide()
+    e.key === "ArrowRight" && nextSlide()
+  })
+
+  dotContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("dots__dot")) {
+      const { slide } = e.target.dataset
+      goToSlide(slide)
+      activateDot(slide)
+    }
+  })
+}
+slider()
